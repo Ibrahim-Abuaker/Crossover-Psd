12ccfb6620f1805ae562c21f45a0e408694da75f
Niloufar Asghari <nilou.asghari@gmail.com>
2024-02-19 13:34:51 +0100

    text part completed

--- src/components/Part2.jsx
+++ src/components/Part2.jsx
@@ -1,12 +1,19 @@
 import Styles from "../Part2.module.css";
 import RotateLogo from "./RotateLogo";
 import "../App.css";
+import TextPart from "./TextPart";
 export default function Part2() {
   return (
-    <>
-      <div className={`${Styles.container}`} style={{ marginTop: "50px" }}>
-        <RotateLogo className="container" />
+    <div
+      className={`${Styles.bg} `}
+      style={{ marginTop: "50px", paddignTop: "10px" }}
+    >
+      <div className={`${Styles.center} `}>
+        <div className={` ${Styles.position}`}>
+          <RotateLogo />
+          <TextPart />
+        </div>
       </div>
-    </>
+    </div>
   );
 }
--- src/components/RotateLogo.jsx
+++ src/components/RotateLogo.jsx
@@ -1,18 +1,16 @@
 import Styles from "../RotateLogo.module.css";
 
-export default function RotateLogo({ className }) {
+export default function RotateLogo() {
   console.log("hi from part 2");
   return (
-    <div className={`${className} `}>
-      <div className={`${Styles.rtWrapper}`}>
-        <img
-          className={`${Styles.rtImg}`}
-          src="/rotationText.png"
-          alt="rotaionText"
-        />
-        <div className={`${Styles.rtIconWrapper}`}>
-          <img className={`${Styles.rtIcon}`} src="/hand.svg" alt="hand" />
-        </div>
+    <div className={`${Styles.rtWrapper}`}>
+      <img
+        className={`${Styles.rtImg}`}
+        src="/rotationText.png"
+        alt="rotaionText"
+      />
+      <div className={`${Styles.rtIconWrapper}`}>
+        <img className={`${Styles.rtIcon}`} src="/hand.svg" alt="hand" />
       </div>
     </div>
   );
