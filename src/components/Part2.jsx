eec9b052323387321c8cfa70ab148881a32e1e53
Niloufar Asghari <nilou.asghari@gmail.com>
2024-02-19 14:13:26 +0100

    video componnent completed

--- src/components/Part2.jsx
+++ src/components/Part2.jsx
@@ -2,6 +2,7 @@ import Styles from "../Part2.module.css";
 import RotateLogo from "./RotateLogo";
 import "../App.css";
 import TextPart from "./TextPart";
+import Video from "./Video";
 export default function Part2() {
   return (
     <div
@@ -13,6 +14,21 @@ export default function Part2() {
           <RotateLogo />
           <TextPart />
         </div>
+        <Video
+          style={{
+            marginTop: "50px",
+            paddingTop: "10px",
+            width: "100%",
+            marginBottom: "50px",
+          }}
+          posterUrl="https://assets-global.website-files.com/6586e5fc525ee55fa1623142/65c311d44fc45f0908cb04e8_bgvideo-poster-00001.jpg"
+          videoUrls={[
+            "https://assets-global.website-files.com/6586e5fc525ee55fa1623142/65c311d44fc45f0908cb04e8_bgvideo-transcode.mp4",
+            "https://assets-global.website-files.com/6586e5fc525ee55fa1623142/65c311d44fc45f0908cb04e8_bgvideo-transcode.webm",
+          ]}
+          autoplay={true}
+          loop={true}
+        />
       </div>
     </div>
   );
